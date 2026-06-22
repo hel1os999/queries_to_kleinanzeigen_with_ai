3f58cbffa14c124739b9bb493459b27406b59fff
ensomnet <hel1osmif@gmail.com>
2026-06-22 20:16:40 +0200

    add docker-compose + changes in main.py

--- main.py
+++ main.py
@@ -7,10 +7,24 @@ from routers import (
     inserate_batch,
     convert_url,
     inserate_by_url,
+    inserat_with_ai
+
+
 )
 from utils.browser import OptimizedPlaywrightManager
 from utils.asyncio_optimizations import EventLoopOptimizer
 
+
+from langchain.agents import create_agent
+
+from ai.tools.ai_query import build_search_tool
+from ai.config.settings import settings
+from ai.config.prompts import load_prompt
+
+from redis.asyncio import Redis
+from langgraph.checkpoint.redis import AsyncRedisSaver
+
+from ai.config.settings import settings
 # Global browser manager instance for sharing across all endpoints
 browser_manager = None
 
@@ -30,10 +44,26 @@ async def lifespan(app: FastAPI):
     browser_manager = OptimizedPlaywrightManager(max_contexts=20, max_concurrent=10)
     await browser_manager.start()
 
+    client = Redis(
+        host=settings.redis_config.host,
+        port=settings.redis_config.port, 
+    )
+
+    saver = AsyncRedisSaver(redis_client=client)
+    await saver.asetup()
+    
+    tool = build_search_tool(browser_manager)
+    agent = create_agent(
+        model=settings.agent_config.model, 
+        tools=[tool], 
+        system_prompt=load_prompt("system"),
+        checkpointer=saver
+        )
+
     # Store browser manager in app state for access by routers
     app.state.browser_manager = browser_manager
     app.state.uvloop_enabled = uvloop_enabled
-
+    app.state.agent = agent
     yield
 
     # Shutdown: Clean up browser resources
@@ -45,7 +75,7 @@ app = FastAPI(version="1.0.0", lifespan=lifespan)
 
 
 @app.get("/")
-async def root():
+async def root(): 
     return {
         "message": "Welcome to the Kleinanzeigen API",
         "endpoints": ["/inserate", "/inserat/{id}", "/inserate-detailed"],
@@ -59,3 +89,4 @@ app.include_router(inserate_detailed.router)
 app.include_router(inserate_batch.router)
 app.include_router(convert_url.router)
 app.include_router(inserate_by_url.router)
+app.include_router(inserat_with_ai.router)
